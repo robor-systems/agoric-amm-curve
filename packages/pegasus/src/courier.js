16f6b17f376b4fb6ef9e58430c00e272f084730a
Michael FIG <mfig@agoric.com>
2021-12-15 12:38:32 -0600

    refactor(pegasus): code cleanups

--- packages/pegasus/src/courier.js
+++ packages/pegasus/src/courier.js
@@ -1,3 +1,4 @@
+// @ts-check
 import { details as X } from '@agoric/assert';
 
 import { AmountMath } from '@agoric/ertp';
@@ -19,7 +20,7 @@ export const getCourierPK = (key, keyToCourierPK) => {
 
   // This is the first packet for this denomination.
   // Create a new Courier promise kit for it.
-  const courierPK = makeOncePromiseKit(() => X`${key} already pegged`);
+  const courierPK = makeOncePromiseKit(X`${key} already pegged`);
 
   keyToCourierPK.init(key, courierPK);
   return courierPK;
--- packages/pegasus/src/types.js
+++ packages/pegasus/src/types.js
@@ -1,3 +1,4 @@
+// @ts-check
 // eslint-disable-next-line spaced-comment
 /// <reference types="ses"/>
 
@@ -86,7 +87,7 @@
  */
 
 /**
- * @typedef {Object} PegasusConnectionState
+ * @typedef {Object} PegasusConnection
  * @property {PegasusConnectionActions} [actions]
  * @property {Address} localAddr
  * @property {Address} remoteAddr
@@ -96,5 +97,5 @@
 /**
  * @typedef {Object} PegasusConnectionKit
  * @property {ConnectionHandler} handler
- * @property {Subscription<PegasusConnectionState>} subscription
+ * @property {Subscription<PegasusConnection>} subscription
  */
